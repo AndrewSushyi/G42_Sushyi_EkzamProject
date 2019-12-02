c526b3cb85a073c4518c770837f4f8bec7562275
AndrewSushyi <sushyi_av@ukr.net>
2019-12-02 21:37:02 +0200

    first commit

---
+++ src/main/java/G42_Sushyi_EkzamProject/App.java
@@ -0,0 +1,13 @@
+package G42_Sushyi_EkzamProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/G42_Sushyi_EkzamProject/AppTest.java
@@ -0,0 +1,20 @@
+package G42_Sushyi_EkzamProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
